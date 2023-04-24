15a82b014d89583eaa46a591f1489b106b399c0e
roodhouse <rughjm@gmail.com>
2023-04-23 20:14:30 -0500

    added bootstrap.js files

---
+++ packages/color-wheel/src/bootstrap.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Color from './components/color/Color';
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <App />,
+    children: [
+      {
+        path: '*color-wheel',
+        element: <Color />,
+      },
+    ],
+  },
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+    <RouterProvider router={router} />
+  // </React.StrictMode>
+);
